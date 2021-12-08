e3f748886913a07932c5c1fe74c11c1a256cbd46
Ni Ngo <ni.ngo@asnet.com.vn>
2021-12-08 19:11:10 +0700

    Add more data and util

---
+++ api/posts/This weekend.md
@@ -0,0 +1,5 @@
+---
+title: Camping vibe
+---
+
+Say hello
--- app/root.tsx
+++ app/root.tsx
@@ -132,12 +132,6 @@ function Layout({ children }: { children: React.ReactNode }) {
               <li>
                 <Link to="/">Home</Link>
               </li>
-              <li>
-                <a href="https://remix.run/docs">Remix Docs</a>
-              </li>
-              <li>
-                <a href="https://github.com/remix-run/remix">GitHub</a>
-              </li>
             </ul>
           </nav>
         </div>
@@ -145,11 +139,6 @@ function Layout({ children }: { children: React.ReactNode }) {
       <div className="remix-app__main">
         <div className="container remix-app__main-content">{children}</div>
       </div>
-      <footer className="remix-app__footer">
-        <div className="container remix-app__footer-content">
-          <p>&copy; You!</p>
-        </div>
-      </footer>
     </div>
   );
 }
