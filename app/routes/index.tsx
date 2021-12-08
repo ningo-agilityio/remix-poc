c83267cbbf798acbb428bc3d9fb642f0bd26baba
Ni Ngo <ni.ngo@asnet.com.vn>
2021-12-08 19:17:44 +0700

    Add posts page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,33 +12,16 @@ type IndexData = {
 // https://remix.run/api/conventions#loader
 export let loader: LoaderFunction = () => {
   let data: IndexData = {
-    resources: [
-      {
-        name: "Remix Docs",
-        url: "https://remix.run/docs"
-      },
-      {
-        name: "React Router Docs",
-        url: "https://reactrouter.com/docs"
-      },
-      {
-        name: "Remix Discord",
-        url: "https://discord.gg/VBePs6d"
-      }
-    ],
+    resources: [],
     demos: [
       {
         to: "demos/actions",
         name: "Actions"
       },
       {
-        to: "demos/about",
-        name: "Nested Routes, CSS loading/unloading"
+        to: "posts",
+        name: "Blogs"
       },
-      {
-        to: "demos/params",
-        name: "URL Params and Error Boundaries"
-      }
     ]
   };
 
@@ -61,21 +44,6 @@ export default function Index() {
   return (
     <div className="remix__page">
       <main>
-        <h2>Welcome to Remix!</h2>
-        <p>We're stoked that you're here. 🥳</p>
-        <p>
-          Feel free to take a look around the code to see how Remix does things,
-          it might be a bit different than what you’re used to. When you're
-          ready to dive deeper, we've got plenty of resources to get you
-          up-and-running quickly.
-        </p>
-        <p>
-          Check out all the demos in this starter, and then just delete the{" "}
-          <code>app/routes/demos</code> and <code>app/styles/demos</code>{" "}
-          folders when you're ready to turn this into your next project.
-        </p>
-      </main>
-      <aside>
         <h2>Demos In This App</h2>
         <ul>
           {data.demos.map(demo => (
@@ -86,15 +54,7 @@ export default function Index() {
             </li>
           ))}
         </ul>
-        <h2>Resources</h2>
-        <ul>
-          {data.resources.map(resource => (
-            <li key={resource.url} className="remix__page__resource">
-              <a href={resource.url}>{resource.name}</a>
-            </li>
-          ))}
-        </ul>
-      </aside>
+      </main>
     </div>
   );
 }
