5741ab53e6fa72b75a58f360785e5809c9224791
Ni Ngo <ni.ngo@asnet.com.vn>
2021-12-10 14:53:05 +0700

    Config route

--- remix.config.js
+++ remix.config.js
@@ -5,5 +5,10 @@ module.exports = {
   appDirectory: "app",
   browserBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildDirectory: "api/build"
+  serverBuildDirectory: "api/build",
+  routes(defineRoutes) {
+    return defineRoutes(route => {
+      route("api/build/posts-data/*", "api/posts-data");
+    });
+  }
 };
