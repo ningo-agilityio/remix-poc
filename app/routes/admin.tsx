1953438a1c78d9ea164636e82815ba93052f5825
Ni Ngo <ni.ngo@asnet.com.vn>
2021-12-13 11:09:40 +0700

    Rever code

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,6 +1,6 @@
 import { Outlet, Link, useLoaderData } from "remix";
-import { getPosts } from "../../api/post";
-import type { Post } from "../../api/post";
+import { getPosts } from "../post";
+import type { Post } from "../post";
 
 import adminStyles from "~/styles/admin.css";
 
--- app/routes/admin/new.tsx
+++ app/routes/admin/new.tsx
@@ -1,7 +1,7 @@
 import { redirect, Form, useActionData, useTransition } from "remix";
-import { createPost } from "../../../api/post";
+import { createPost } from "../../post";
 import type { ActionFunction } from "remix";
-import type { NewPost } from "../../../api/post";
+import type { NewPost } from "../../post";
 import invariant from "tiny-invariant";
 
 type PostError = {
